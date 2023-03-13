05c21de40b6fef3c87c51b761359af49259e957f
Mehti Sadixov <82047091+mexdi008@users.noreply.github.com>
2023-03-13 12:59:33 +0400

    mysql_springboot_crud

---
+++ Mysql-Spring-Boot-example/src/main/java/com/mehdi/Mysql/MysqlSpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mehdi.Mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlSpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlSpringBootExampleApplication.class, args);
+	}
+
+}
---
+++ Mysql-Spring-Boot-example/src/test/java/com/mehdi/Mysql/MysqlSpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehdi.Mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlSpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
